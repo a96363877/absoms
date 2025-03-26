9726bb1215d2360d806e081f7e882e5f564f218b
a96363877 <a96363877@gmail.com>
2025-03-25 23:35:14 -0700

    Update firestore.ts

--- lib/firestore.ts
+++ lib/firestore.ts
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD9M4S1blhwbJQ5FUXBkqzv5lsE_3QR4mQ",
-  authDomain: "amer-98afb.firebaseapp.com",
-  projectId: "amer-98afb",
-  storageBucket: "amer-98afb.firebasestorage.app",
-  messagingSenderId: "492435134855",
-  appId: "1:492435134855:web:86d3abd762bdc1856e5b22",
-  measurementId: "G-1YBB0DGJ9L"
+   apiKey: "AIzaSyBoqC6S8FvbWhud_PnD6IC4pF61iffe4tI",
+  authDomain: "qatar22-d0e6a.firebaseapp.com",
+  projectId: "qatar22-d0e6a",
+  storageBucket: "qatar22-d0e6a.firebasestorage.app",
+  messagingSenderId: "349776944758",
+  appId: "1:349776944758:web:6a8dd2558e8ac248570957",
+  measurementId: "G-079WEHH5KL"
 };
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
